38263c03c63c96ee5e217b8eba5ed42655588f5b
awp-1 <shekharanand077@gamil.com>
2026-07-10 00:20:10 +0530

    chore: initialize Spring Boot project structure with dependencies

---
+++ task-management-service/src/test/java/com/albertsons/assignment/taskservice/TaskManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albertsons.assignment.taskservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
